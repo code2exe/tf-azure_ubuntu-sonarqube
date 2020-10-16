efb4b2eb619a21f7a56d6fab7ce7a03942575ec3
Henry <16284830+code2exe@users.noreply.github.com>
2020-10-16 20:37:23 +0100

    Completed required Terraform scripts

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "subscription_id" {
+  type = string
+  description = "Azure Subscription ID"
+}
+variable "client_id" {
+  type = string
+  description = "Azure Client ID"
+}
+variable "client_secret" {
+  type = string
+  description = "Azure Client Secret"
+}
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+}
+variable "location" {
+  description = "Azure Default location"
+}
+variable "admin_username" {
+  description = "Azure VM Username"
+}
